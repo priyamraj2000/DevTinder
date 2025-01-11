f5970886c320c78ed3fc63ad02a83d6651e686d6
Priyam Raj <priyamraj@MacBook-Air.local>
2025-01-11 23:23:15 +0530

    Dynamic post api

--- src/app.js
+++ src/app.js
@@ -3,28 +3,80 @@ const connectDB = require("./config/database");
 const User = require("./model/user.js");
 const app = express();
 
-app.post("/signup", async (req,res)=>{
-    console.log(req.body);  
-  //const user = new User({
-//     firstName: "Sachin",
-//     lastName: "Tendulkar",
-//     emailId: "sachin@kohli.com",
-//     password : "sachin@123",
-//   });
-//   try {
-//     await user.save();
-//     res.send("USer Added successfully!");
-//   } catch(err){
-//     res.status(400).send("Error saving the user:" + err.message);
-//   }
-});
+app.use(express.json());
 
-// app.use(express.json());
+app.post ("/signup",async(req,res)=>{
+    
+    const user= new User(req.body);
+   try{
+   await user.save();
+   res.send("New User added")
+   }
+   catch(err){
+    res.status(404).send("something went wrong");
+   }
 
-// app.post("/signup", async (req,res) =>{
-//   console.log(req.body);    
-// });  
+})
+
+connectDB()
+    .then(()=>{
+    console.log("Database connection Establised");
+    app.listen(7777,()=>{
+        console.log("Server is successfully listening on port 7777..")
+    });
+    })
+    .catch((err)=>{
+        console.log("Database cannot be connected");
+    });
 
 
- app.listen(3001, console.log("listening"));
+// app.post("/signup", async (req,res)=>{
+//     console.log(req.body);  
+//   //const user = new User({
+// //     firstName: "Sachin",
+// //     lastName: "Tendulkar",
+// //     emailId: "sachin@kohli.com",
+// //     password : "sachin@123",
+// //   });
+// //   try {
+// //     await user.save();
+// //     res.send("USer Added successfully!");
+// //   } catch(err){
+// //     res.status(400).send("Error saving the user:" + err.message);
+// //   }
+// });
+
+// // app.use(express.json());
+
+// // app.post("/signup", async (req,res) =>{
+// //   console.log(req.body);    
+// // });  
+
+// app.get("/user",(req,res,next)=>{
+//     console.log("First route");
+//     next();
+// },
+//  (req,res,next)=>{
+//     console.log("seconf route");
+//     next();
+//  },
+//  (req,res)=>{
+//     console.log("Third route");
+    
+    
+//  }
+// )
+
+// // app.get("/user",(req,res)=>{
+// //     res.send({firstName:"Priyam", secondName:"Raj"});
+// // });
+
+// app.post("/user", (req,res)=>{
+//     res.send("post call connected");
+// })
+
+
+//  app.listen(3001, console.log("listening"));
+
+
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,52 +1,47 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const connectDB = async() =>{
-//     await mongoose.connect(
-//         "mongodb+srv://priyamraj2001:w6O139oledYsT2Ne@namastenode.y50oe.mongodb.net/DevTinder"
-//     );
-// };
+const connectDB = async() =>{
+    await mongoose.connect(
+        "mongodb+srv://priyamraj2001:w6O139oledYsT2Ne@namastenode.y50oe.mongodb.net/DevTinder"
+    );
+};
 
-// connectDB().then(()=>{
-//     console.log("Database connected to the server");
-// }).catch(err=>{
-//     console.error("Database cannot be connected");
-//})
+module.exports = connectDB;
+// const {MongoClient} = require("mongodb");
 
-const {MongoClient} = require("mongodb");
+// // Connection URL
+// const url = 'mongodb+srv://priyamraj2001:w6O139oledYsT2Ne@namastenode.y50oe.mongodb.net/DevTinder';
+// const client = new MongoClient(url);
 
-// Connection URL
-const url = 'mongodb+srv://priyamraj2001:w6O139oledYsT2Ne@namastenode.y50oe.mongodb.net/DevTinder';
-const client = new MongoClient(url);
+// // Database Name
+// const dbName = "HelloWorld";
 
-// Database Name
-const dbName = "HelloWorld";
-
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('user');
-  const data= {
-    firstname: "Deepak",
-    lastname: "Raut",
-    city: "Guwahati",
-    phoneNumber: "92373732732"
-  }
+// async function main() {
+//   // Use connect method to connect to the server
+//   await client.connect();
+//   console.log('Connected successfully to server');
+//   const db = client.db(dbName);
+//   const collection = db.collection('user');
+//   const data= {
+//     firstname: "Deepak",
+//     lastname: "Raut",
+//     city: "Guwahati",
+//     phoneNumber: "92373732732"
+//   }
 
   
   
-  const findResult = await collection.find({}).toArray();
-   console.log('Found documents =>', findResult);
-  // the following code examples can be pasted here...
-  const count = await collection.find({"First Name" :"Deepika"}).toArray();
-  console.log("found object =>", count);
-  return 'done.';
-}
+//   const findResult = await collection.find({}).toArray();
+//    console.log('Found documents =>', findResult);
+//   // the following code examples can be pasted here...
+//   const count = await collection.find({"First Name" :"Deepika"}).toArray();
+//   console.log("found object =>", count);
+//   return 'done.';
+// }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+// main()
+//   .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close());
 
 
